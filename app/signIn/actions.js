63417f4d0eace2440e97964e4425c03aa495bb78
jalee314 <jasonklee1003@gmail.com>
2025-04-28 16:12:58 -0700

    fixed documentaiton code to work, should be compatible

--- app/signIn/actions.js
+++ app/signIn/actions.js
@@ -1,9 +1,9 @@
 'use server'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { createClient } from '../../utils/supabase/server'
+import { createSupabaseServerClient } from '../../utils/supabase/server'
 export async function login(formData) {
-  const supabase = await createClient()
+  const supabase = await createSupabaseServerClient()
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
@@ -18,7 +18,7 @@ export async function login(formData) {
   redirect('/account')
 }
 export async function signup(formData) {
-  const supabase = await createClient()
+  const supabase = await createSupabaseServerClient()
   const data = {
     email: formData.get('email'),
     password: formData.get('password'),
