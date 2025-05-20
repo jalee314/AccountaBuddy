e0bd55632c672948f437500e4c899ed625c52550
jalee314 <jasonklee1003@gmail.com>
2025-05-19 23:08:41 -0700

    removed fiels

--- app/signIn/page.jsx
+++ app/signIn/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Navbar from '../../components/Navbar';
-import SignInView from '../../components/SignInView';
+import SignInView from '../../src/views/SignInView';
 
 export default function SignInPage() {
   return (
